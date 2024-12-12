e22be23f355c46b9709fb9cc0da09c3503b8fe76
Leon Graumans <l.graumans@labdigital.nl>
2024-12-12 21:36:12 +0100

    chore: remove commented code

--- src/lib/help-text.ts
+++ src/lib/help-text.ts
@@ -45,35 +45,3 @@ export const outputHelp = () => {
   const usage = commandLineUsage(sections);
   console.info(usage);
 };
-
-// export const outputHelp = () => {
-//   const sections: Section[] = [
-//     {
-//       header: "SQUASHY",
-//       content: "The Strava Squash Sync Service.",
-//     },
-//     {
-//       header: "Options",
-//       optionList: [
-//         {
-//           name: "help",
-//           description: "Display this usage guide.",
-//           alias: "h",
-//           type: Boolean,
-//         },
-//         {
-//           name: "offet",
-//           description:
-//             "The offset for retrieving Strava activities in hours (default: 1).",
-//           alias: "o",
-//           type: Number,
-//           typeLabel: "{underline hours}",
-//           defaultValue: 1,
-//         },
-//       ],
-//     },
-//   ];
-
-//   const usage = commandLineUsage(sections);
-//   console.info(usage);
-// };
